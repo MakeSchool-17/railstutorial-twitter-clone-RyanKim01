7f94e392fcbfd5a3b19b0900aa152333f97cab52
RyanKim01 <kyhekm@hotmail.com>
2016-02-01 22:45:31 -0800

    Implemented integration tests for log in and log out

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -11,8 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-  
+
   test "valid signup information" do
    get signup_path
    assert_difference 'User.count', 1 do
@@ -22,5 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password_confirmation: "password" }
    end
    assert_template 'users/show'
+   assert_not_empty flash
+   assert is_logged_in?
  end
 end
